e21b1378ceb935e7d335212487a824aefaaa0dc3
wr1 <8971152+wr1@users.noreply.github.com>
2025-12-09 11:04:07 +0100

    Update __init__.py to include laminate

--- src/b3_mat/__init__.py
+++ src/b3_mat/__init__.py
@@ -1,11 +1,11 @@
 """b3_mat: Library for composite material data handling."""
 
-from .materials import IsotropicMaterial, OrthotropicMaterial, MaterialDB
 from .laminate import calculate_laminate_properties
+from .materials import IsotropicMaterial, MaterialDB, OrthotropicMaterial
 
 __all__ = [
     "IsotropicMaterial",
-    "OrthotropicMaterial",
     "MaterialDB",
+    "OrthotropicMaterial",
     "calculate_laminate_properties",
 ]
