c49a91c96c39bc75c176191898f58032c846f3c9
wr1 <8971152+wr1@users.noreply.github.com>
2025-12-09 11:04:08 +0100

    Update calculix.py

--- src/b3_mat/calculix.py
+++ src/b3_mat/calculix.py
@@ -7,21 +7,17 @@ from typing import List
 
 import numpy as np
 
-from .materials import MaterialDB
-
 logger = logging.getLogger(__name__)
 
 
 def material_db_to_ccx(
-    materials: List[float],
-    matmap: str,
-    force_iso: bool = False
+    materials: List[float], matmap: str, force_iso: bool = False
 ) -> str:
     """Generate Calculix material block from material map."""
     if not os.path.isfile(matmap):
         raise FileNotFoundError("No material map defined")
 
-    with open(matmap, "r") as f:
+    with open(matmap) as f:
         mm1 = json.load(f)
         mm = mm1["map"]
         mat_db = mm1["matdb"]
@@ -32,7 +28,9 @@ def material_db_to_ccx(
     for i in materials:
         if i > 1e-6:
             material_properties = mat_db[mm_inv[int(i)]]
-            matblock += f"** material: {mm_inv[int(i)]} {i} {material_properties['name']}\n"
+            matblock += (
+                f"** material: {mm_inv[int(i)]} {i} {material_properties['name']}\n"
+            )
 
             if "C" in material_properties and not force_iso:
                 logger.info(f"{material_properties['name']} is orthotropic")
@@ -49,15 +47,17 @@ def material_db_to_ccx(
                 D[3, 3] = C[5, 5]
                 D[5, 5] = C[3, 3]
                 matblock += (
-                    f"{D[0,0]:.4g},{D[0,1]:.4g},{D[1,1]:.4g},"
-                    f"{D[0,2]:.4g},{D[1,2]:.4g},{D[2,2]:.4g},"
-                    f"{D[3,3]:.4g},{D[4,4]:.4g},\n"
-                    f"{D[5,5]:.4g},293\n"
+                    f"{D[0, 0]:.4g},{D[0, 1]:.4g},{D[1, 1]:.4g},"
+                    f"{D[0, 2]:.4g},{D[1, 2]:.4g},{D[2, 2]:.4g},"
+                    f"{D[3, 3]:.4g},{D[4, 4]:.4g},\n"
+                    f"{D[5, 5]:.4g},293\n"
                 )
             elif "Ex" in material_properties and not force_iso:
                 logger.info(f"{material_properties['name']} has engineering constants")
                 matblock += "** orthotropic material\n"
-                matblock += f"*material,name=m{int(i)}\n*elastic,type=engineering constants\n"
+                matblock += (
+                    f"*material,name=m{int(i)}\n*elastic,type=engineering constants\n"
+                )
                 matblock += (
                     f"{material_properties['Ex']:.4g},{material_properties['Ey']:.4g},{material_properties['Ez']:.4g},"
                     f"{material_properties['nuxy']:.4g},{material_properties['nuxz']:.4g},{material_properties['nuyz']:.4g},"
@@ -70,7 +70,9 @@ def material_db_to_ccx(
                     0.45,
                     max(
                         0.1,
-                        material_properties.get("nu", material_properties.get("nuxy", 0.3)),
+                        material_properties.get(
+                            "nu", material_properties.get("nuxy", 0.3)
+                        ),
                     ),
                 )
                 E = material_properties.get("Ex", material_properties.get("E", 1e9))
