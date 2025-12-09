9f64edf705336e25240cc268455a0cbd2af2a763
wr1 <8971152+wr1@users.noreply.github.com>
2025-12-09 11:03:15 +0100

    Add laminate.py for laminate theory calculations using lamprop

---
+++ src/b3_mat/laminate.py
@@ -0,0 +1,42 @@
+"""Laminate theory calculations using lamprop."""
+
+from typing import List, Tuple
+
+import numpy as np
+from lamprop import Laminate
+
+from .materials import OrthotropicMaterial
+
+
+def calculate_laminate_properties(
+    layers: List[Tuple[OrthotropicMaterial, float, float]]
+) -> dict:
+    """Calculate laminate properties using lamprop.
+
+    Args:
+        layers: List of (material, thickness, angle) tuples.
+
+    Returns:
+        Dict with A, B, D matrices and other properties.
+    """
+    lam = Laminate()
+    for mat, t, angle in layers:
+        lam.add_layer(
+            E1=mat.Ex,
+            E2=mat.Ey,
+            v12=mat.nuxy,
+            G12=mat.Gxy,
+            thickness=t,
+            angle=angle
+        )
+    return {
+        "A": lam.A,
+        "B": lam.B,
+        "D": lam.D,
+        "ABD": lam.ABD,
+        "thickness": lam.thickness,
+        "Ex": lam.Ex,
+        "Ey": lam.Ey,
+        "Gxy": lam.Gxy,
+        "nuxy": lam.vxy,
+    }
