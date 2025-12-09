412c9ec1ab688e5b8ca9b19a2f5308285269542c
wr1 <8971152+wr1@users.noreply.github.com>
2025-12-09 11:03:01 +0100

    Add __init__.py for b3_mat package

---
+++ src/b3_mat/__init__.py
@@ -0,0 +1,5 @@
+"""b3_mat: Library for composite material data handling."""
+
+from .materials import IsotropicMaterial, OrthotropicMaterial, MaterialDB
+
+__all__ = ["IsotropicMaterial", "OrthotropicMaterial", "MaterialDB"]
